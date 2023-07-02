222335b91a14024364d2d723fa66becd7eebee22
Lastdragon04 <1692930439@qq.com>
2023-07-02 11:29:40 +0800

    弹窗register1基本完成js未编写

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from flask_mail import Mail
 from config import Config
 from exts import mail
 from apps.user import bp as user_bp
 
-
-app=Flask(__name__)
+app = Flask(__name__)
 app.config.from_object(Config)
 mail.init_app(app)
 app.register_blueprint(user_bp)
 app.jinja_env.variable_start_string = '(('
 app.jinja_env.variable_end_string = '))'
-@app.route('/',methods=['GET'])
+
+
+@app.route('/', methods=['GET'])
 def index():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('index.html')
