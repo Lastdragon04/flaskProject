e4a3ed711b7b63ae318834b853d0cd594042d88c
Lastdragon04 <1692930439@qq.com>
2023-07-07 00:10:51 +0800

    他妈的提交的时候不知道按哪了，给老子整了这么多乱码，现在改完了阿

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-
 import smtplib
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, session
 from config import Config
 from flask_migrate import Migrate
 from exts import mail, System_name, db
-from Model import UserModel
-from Mail import send_captcha
+
 from apps.user import bp as user_bp
-from bll import create_captcha
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -27,39 +24,8 @@ def index():
         return render_template('index.html')
 
 
-@app.route('/send_captcha', methods=['POST'])
-def send_captcha():
-    captcha = create_captcha(6)
-    Register_Email = request.form.getlist('Register_Email')
-    Register_Username = request.form['Register_Username']
-    try:
-        X = send_captcha(Register_Username, System_name, captcha, Register_Email)
-        session['Register_Email_session'] = Register_Email[0]
-        session['Register_Username'] = Register_Username
-        session['captcha'] = captcha
-    except smtplib.SMTPServerDisconnected:
-        return 'Wrong'
-    return 'OK'
-
-
 @app.context_processor
 def my_context_processor():
     return {"System_name": System_name}
 
-from flask import Flask,render_template,request
-from flask_mail import Mail
-from config import Config
-from exts import mail
-from apps.user import bp as user_bp
-
-
-app=Flask(__name__)
-app.config.from_object(Config)
-mail.init_app(app)
-app.register_blueprint(user_bp)
-
-@app.route('/index',methods=['GET'])
-def index():
-    if request.method=='GET':
-        return render_template('../templates/index.html')
 
--- apps/user.py
+++ apps/user.py
@@ -7,9 +7,7 @@ from Mail import send_captcha
 from SQL_Defence import SQL_defend
 from Model import UserModel
 
-
 bp = Blueprint('user', __name__, url_prefix='/user')
-
 
 
 @bp.route('/register1_send_captcha', methods=['POST'])
@@ -79,7 +77,3 @@ def Login():
                     return 'Error'
         else:
             return 'Wrong'
-@bp.route('/register1', methods=['POST'])
-def register1():
-    return
-
